67f940c084f837813b448aee4aedc4a0673b28b1
wmesonal <wmesonal@gmail.com>
2026-09-10 16:51:39 +0530

    login page issue resolved

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/kohali-logo.png";
 import {
