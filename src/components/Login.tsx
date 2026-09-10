3c4271eae3b8f3679ed1033ae9c13541cdba3249
wmesonal <wmesonal@gmail.com>
2026-09-10 16:28:40 +0530

    run build

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -167,7 +167,7 @@ export function LoginPage() {
   }
 
 
-  const API_PATH = import.meta.env.VITE_LOCAL_API_PATH;
+  const API_PATH = import.meta.env.VITE_LIVE_API_PATH;
   async function handleAdminLogin() {
     if (!adminValid || isLoading) return;
     setIsLoading(true);
